ecc58b29db854f875eaaa7efabc5f7d3a96d5267
Marharyta <mays777i.ua@gmail.com>
2026-03-25 00:40:10 +0200

    Practical 2 initial commit

---
+++ Task1/SmartHomeObserver.cs
@@ -0,0 +1,82 @@
+﻿namespace Task1;
+
+class SmartHomeObserver
+{
+    public static void Main(string[] args)
+    {
+        TemperatureSensor sensor = new TemperatureSensor();
+
+        Display display = new Display();
+        AirConditioner ac = new AirConditioner();
+        SecuritySystem security = new SecuritySystem();
+
+        sensor.TemperatureChanged += display.OnTemperatureChanged;
+        sensor.TemperatureChanged += ac.OnTemperatureChanged;
+        sensor.TemperatureChanged += security.OnTemperatureChanged;
+
+        sensor.SetTemperature(15);
+        sensor.SetTemperature(20);
+        sensor.SetTemperature(27);
+        sensor.SetTemperature(42);
+        sensor.SetTemperature(3);
+    }
+}
+
+public class TemperatureSensor
+{
+    public delegate void TemperatureChangedEventHandler(double temperature);
+
+    public event TemperatureChangedEventHandler TemperatureChanged;
+
+    private double _temperature;
+
+    public void SetTemperature(double temperature)
+    {
+        _temperature = temperature;
+        Console.WriteLine($"\nTemperature changed to {_temperature}°C");
+
+        TemperatureChanged?.Invoke(_temperature);
+    }
+}
+
+public class Display
+{
+    public void OnTemperatureChanged(double temperature)
+    {
+        Console.WriteLine($"Display: Current temperature is {temperature}°C");
+    }
+}
+
+public class AirConditioner
+{
+    public void OnTemperatureChanged(double temperature)
+    {
+        if (temperature < 17)
+        {
+            Console.WriteLine("AirConditioner: Heating ON");
+        }
+        else if (temperature <= 25)
+        {
+            Console.WriteLine("AirConditioner: AC OFF");
+        }
+        else
+        {
+            Console.WriteLine("AirConditioner: Cooling ON");
+        }
+    }
+}
+
+public class SecuritySystem
+{
+    public void OnTemperatureChanged(double temperature)
+    {
+        if (temperature > 40)
+        {
+            Console.WriteLine("SecuritySystem: WARNING! Overheating detected!");
+        }
+        if (temperature < 5)
+        {
+            Console.WriteLine("SecuritySystem: WARNING! Freezing risk detected!");
+        }
+    }
+}
---
+++ Task2/GameObserver.cs
@@ -0,0 +1,99 @@
+﻿namespace Task2;
+
+class GameObserver
+{
+    public static void Main(string[] args)
+    {
+        Player player = new Player(100);
+
+        UIHealthBar ui = new UIHealthBar();
+        SoundSystem sound = new SoundSystem();
+        AchievementSystem achievements = new AchievementSystem();
+        GameLogger logger = new GameLogger();
+
+        player.DamageTaken += ui.OnDamageTaken;
+        player.DamageTaken += sound.OnDamageTaken;
+        player.DamageTaken += achievements.OnDamageTaken;
+        player.DamageTaken += logger.OnDamageTaken;
+
+        player.TakeDamage(10);
+        player.TakeDamage(30);
+        player.TakeDamage(25);
+        player.TakeDamage(15);
+        player.TakeDamage(20);
+    }
+}
+
+public class Player
+{
+    public delegate void DamageTakenEventHandler(int damage, int currentHP);
+
+    public event DamageTakenEventHandler DamageTaken;
+
+    private int _hp;
+
+    public Player(int initialHP)
+    {
+        _hp = initialHP;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        _hp -= damage;
+        if (_hp < 0)
+            _hp = 0;
+
+        Console.WriteLine($"\nPlayer takes {damage} damage");
+
+        DamageTaken?.Invoke(damage, _hp);
+    }
+}
+
+public class UIHealthBar
+{
+    public void OnDamageTaken(int damage, int currentHP)
+    {
+        Console.WriteLine($"UIHealthBar: HP = {currentHP}");
+    }
+}
+
+public class SoundSystem
+{
+    public void OnDamageTaken(int damage, int currentHP)
+    {
+        Console.WriteLine("SoundSystem: Play damage sound");
+
+        if (currentHP <= 20)
+        {
+            Console.WriteLine("SoundSystem: CRITICAL HP sound");
+        }
+    }
+}
+
+public class AchievementSystem
+{
+    private bool halfHealthUnlocked = false;
+    private bool firstDeathUnlocked = false;
+
+    public void OnDamageTaken(int damage, int currentHP)
+    {
+        if (!halfHealthUnlocked && currentHP <= 50)
+        {
+            halfHealthUnlocked = true;
+            Console.WriteLine("Achievement: Half Health");
+        }
+        if (!firstDeathUnlocked && currentHP <= 0)
+        {
+            firstDeathUnlocked = true;
+            Console.WriteLine("Achievement: First Death");
+        }
+    }
+}
+
+public class GameLogger
+{
+    public void OnDamageTaken(int damage, int currentHP)
+    {
+        Console.WriteLine($"GameLogger: Damage = {damage}, Current HP = {currentHP}");
+    }
+}
